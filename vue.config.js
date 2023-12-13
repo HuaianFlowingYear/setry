3c469993c6dadb682d9a4805d11d351edcb70eba
jiangliu <1135681182@.com>
2023-12-13 15:11:37 +0800

    成功

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    configureWebpack: {
+        devtool: 'source-map',
+        plugins: [
+            sentryWebpackPlugin({
+                org: 'huaian',
+                project: 'vue-creey',
+                authToken: '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',
+            })
+        ]
+    }
 })
