2546b7e6d5e33fb93ef301e2ceed1907afbcdefb
jiangliu <1135681182@.com>
2023-12-13 14:35:40 +0800

    集成sentry

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Router from "vue-router";
+import * as Sentry from "@sentry/vue";
+Vue.use(Router);
+
+Vue.config.productionTip = false
+
+Sentry.init({
+    Vue,
+    dsn: "https://1e142fc416213d555bba29519747dad2@o4506386926600192.ingest.sentry.io/4506386975686658",
+    integrations: [
+        new Sentry.BrowserTracing({
+            tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+        }),
+        new Sentry.Replay(),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
+});
+
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app')
